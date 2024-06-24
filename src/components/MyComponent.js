a943a47224e8f4cb00696e06830b62fabc9d6e7f
son nguyen <hnson2100254@student.ctuet.edu.vn>
2024-06-25 06:55:11 +0700

    react buoi 2

---
+++ src/components/MyComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+class MyComponent extends React.Component { 
+   // jsx
+    render(){
+        return (
+            <div>My first Component</div>
+        );
+    }
+}
+export default MyComponent;
